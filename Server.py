c81f82fc24abea20642009d5a73c0dd704e3cba7
UVR_JJW_PC\UVR-KAIST <zkrkwlek@gmail.com>
2021-01-24 17:25:10 +0900

    #210124 진행 중

--- Server.py
+++ Server.py
@@ -1,3 +1,6 @@
+
+##################################################
+##import super glue and super point
 import ujson
 import time
 import numpy as np
@@ -15,6 +18,17 @@ import torch
 from superglue.matching import Matching
 from superglue.utils import (AverageTimer, VideoStreamer,
                           make_matching_plot_fast, frame2tensor, keyframe2tensor)
+##import super glue and super point
+##################################################
+
+##################################################
+##import MiDaS
+from torchvision.transforms import Compose
+from midas.midas_net import MidasNet
+from midas.midas_net_custom import MidasNet_small
+from midas.transforms import Resize, NormalizeImage, PrepareForNet
+##import MiDaS
+##################################################
 
 from matplotlib import gridspec
 from matplotlib import pyplot as plt
@@ -22,10 +36,6 @@ from matplotlib import pyplot as plt
 ##################################################
 # API part
 ##################################################
-#cv2.namedWindow('detect', cv2.WINDOW_NORMAL)
-#cv2.resizeWindow('detect', (640,480))
-
-import random
 
 global FrameData
 FrameData = {}
@@ -33,6 +43,71 @@ FrameData = {}
 app = Flask(__name__)
 #cors = CORS(app)
 #CORS(app, resources={r'*': {'origins': ['143.248.96.81', 'http://localhost:35005']}})
+
+@app.route("/api/receiveImage", methods=['POST'])
+def receiveImage():
+    global FrameData
+    start = time.time()
+    params = ujson.loads(request.data)
+    img_encoded = base64.b64decode(params['img'])
+
+    width = int(params['w'])
+    height = int(params['h'])
+    channel = int(params['c'])
+    id = int(params['id'])
+
+    # Convert PIL Image
+    ######
+    img_array = np.frombuffer(img_encoded, dtype=np.uint8)
+    img_cv = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    img_cv = cv2.cvtColor(img_cv, cv2.BGR2RGB)
+    img_resized = cv2.resize(img_cv, dsize=(width/2, height/2))
+    img_gray = cv2.cvtColor(img_cv, cv2.RGB2GRAY)
+
+    temp = {}
+    temp['image'] = img_gray
+    temp['resize'] = img_resized
+    FrameData[id] = temp
+
+    json_data = ujson.dumps({'res': 0})
+    print("Data %d, time : %f" % (len(FrameData), time.time() - start))
+    return json_data;
+@app.route("/api/depthestimate", methods=['POST'])
+def depthestimate():
+    start = time.time()
+    params = ujson.loads(request.data)
+    img_encoded = base64.b64decode(params['img'])
+
+    width = int(params['w'])
+    height = int(params['h'])
+    channel = int(params['c'])
+    id = int(params['id'])
+
+    # Convert PIL Image
+    img_array = np.frombuffer(img_encoded, dtype=np.uint8)
+    img_cv = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+    input_batch = transform(img).to(device)
+
+    with torch.no_grad():
+        prediction = midas(input_batch)
+
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=img.shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze().cpu().numpy()
+
+    h = prediction.shape[0]
+    w = prediction.shape[1]
+    prediction = np.reshape(prediction, w*h)
+    res_str = ' '.join(str(i) for i in prediction)
+
+    json_data = ujson.dumps({'res':res_str , 'w':w, 'h':h})
+    print("Depth Time spent handling the request: %f, %d" % (time.time() - start, 0))
+    return json_data;
+
 @app.route("/api/reset", methods=['POST'])
 def reset():
     global FrameData
@@ -56,10 +131,10 @@ def detect():
     # Convert PIL Image
     ######
     img_array = np.frombuffer(img_encoded, dtype=np.uint8)
-    img_cv = cv2.imdecode(img_array, 1)
-    if channel == 3:
-        print('Receive Color Image')
-    img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
+    img_cv = cv2.imdecode(img_array, cv2.IMREAD_GRAYSCALE)
+    #if channel == 3:
+    #    print('Receive Color Image')
+    #img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
     frame_tensor = frame2tensor(img_cv, device)
     last_data = matching.superpoint({'image': frame_tensor})
 
@@ -102,11 +177,30 @@ def match():
 # END API part
 ##################################################
 
+
+
+
 if __name__ == "__main__":
 
+    ##################################################
+    ##arguments
     parser = argparse.ArgumentParser(
         description='SuperGlue demo',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    #midas
+    """
+    parser.add_argument('--model_path',
+                        default='model-f6b98070.pt',
+                        help='path to the trained weights of model'
+                        )
+
+    parser.add_argument('--model_type',
+                        default='large',
+                        help='model type: large or small'
+                        )
+    """
+    #super glue and point
+
     parser.add_argument(
         '--input', type=str, default='0',
         help='ID of a USB webcam, URL of an IP camera, '
@@ -162,8 +256,43 @@ if __name__ == "__main__":
         help='Force pytorch to run in CPU mode.')
 
     opt = parser.parse_args()
-    #print(opt)
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
+    ###LOAD MIDAS
+
+    midas = torch.hub.load("intel-isl/MiDaS", "MiDaS")
+    midas.to(device)
+    midas.eval()
+    midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+    transform = midas_transforms.default_transform
+
+    """
+    if opt.model_type == "large":
+        midas = MidasNet(opt.model_path, non_negative=True)
+        net_w, net_h = 384, 384
+    elif opt.model_type == "small":
+        midas = MidasNet_small(opt.model_path, features=64, backbone="efficientnet_lite3", exportable=True,
+                               non_negative=True, blocks={'expand': True})
+        net_w, net_h = 256, 256
+    transform = Compose(
+        [
+            Resize(
+                net_w,
+                net_h,
+                resize_target=None,
+                keep_aspect_ratio=True,
+                ensure_multiple_of=32,
+                resize_method="upper_bound",
+                image_interpolation_method=cv2.INTER_CUBIC,
+            ),
+            NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            PrepareForNet(),
+        ]
+    )
+    midas.eval()
+    midas.to(device)
+    """
+    ##LOAD SuperGlue & SuperPoint
     if len(opt.resize) == 2 and opt.resize[1] == -1:
         opt.resize = opt.resize[0:1]
     if len(opt.resize) == 2:
@@ -176,7 +305,7 @@ if __name__ == "__main__":
     else:
         raise ValueError('Cannot specify more than two integers for --resize')
 
-    device = 'cuda' if torch.cuda.is_available() and not opt.force_cpu else 'cpu'
+    #device = 'cuda' if torch.cuda.is_available() and not opt.force_cpu else 'cpu'
     print('Running inference on device \"{}\"'.format(device))
 
     config = {
@@ -193,6 +322,8 @@ if __name__ == "__main__":
     }
     matching = Matching(config).eval().to(device)
     keys = ['keypoints', 'scores', 'descriptors']
+    ##Super Glue and Super Point
+    ##################################################
 
     print('Starting the API')
     app.run(host='127.0.0.1', port = 35005)
