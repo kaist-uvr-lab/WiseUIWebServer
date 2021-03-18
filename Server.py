12e7768c6d26aa7302652303239872514d9e3be6
zkrkwlek_uvr <zkrkwlek@gmail.com>
2021-03-18 17:23:32 +0900

    #210318 facade server

--- Server.py
+++ Server.py
@@ -14,8 +14,7 @@ import argparse
 import torch
 import os
 
-from superglue.matching import Matching
-from superglue.utils import (frame2tensor, keyframe2tensor)
+
 ##import super glue and super point
 ##################################################
 import pickle
@@ -113,10 +112,12 @@ def work2(conv2, queue):
         for messenger in PreprocessingServerList:
             messenger.setData(addr2, message.data)
             messenger.run()
+        end = time.time()
+        print("data send id = %d %f"%(lastID, end-start))
         #requests.post(depthserver_addr+addr2, message.data)
         #requests.post(semanticserver_addr+"?map="+map+"&id="+str(lastID), message.data)
 
-def work1(condition1, condition2, SuperPointAndGlue, queue, queue2):
+def work1(condition1, condition2, queue, queue2):
     print("Start Message Processing Thread")
     while True:
         condition1.acquire()
@@ -125,38 +126,18 @@ def work1(condition1, condition2, SuperPointAndGlue, queue, queue2):
         condition1.release()
         ##### 처리 시작
         start = time.time()
-        img_array = np.frombuffer(message.data, dtype=np.uint8)
-        img_cv = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        img = cv2.cvtColor(img_cv, cv2.COLOR_RGB2GRAY)
-        frame_tensor = frame2tensor(img, device0)
-
-        time1 = time.time()
-        last_data = SuperPointAndGlue.superpoint({'image': frame_tensor})
-        time2 = time.time()
-        desc = last_data['descriptors'][0].cpu().detach().numpy()
-        kpts = last_data['keypoints'][0].cpu().detach().numpy()
-        #scores = last_data['scores'][0].cpu().detach().numpy()
-        end1 = time.time()
 
         user = UserData[message.user]
         Frame = {}
-        Frame['descriptors'] = desc  # descriptor를 미리 트랜스 포즈하고나서 수퍼글루를 더 적게 쓰면 그 때만 트랜스 포즈 하도록 하게 하자.
-        Frame['bkpts'] = kpts.tobytes()
-        Frame['bdesc'] = desc.transpose().tobytes()
         Frame['bimage'] = message.data #인코딩된 바이트 형태
         Frame['binfo'] = user.info
-        #Frame['image'] = img
-        #Frame['scores'] = scores
-        #Frame['keypoints'] = kpts  # last_data['keypoints'][0].cpu().detach().numpy() #에러가능성
         ##id 관리 및 user 정보에 추가
         lastid = MapData[message.map].AddFrame(Frame)
         user.AddData(lastid, message.timestamp)
         message.id = lastid
-        n = len(kpts)
-        ##mapping server에 전달
-        #MapData[message.map].Frames[message.id] = Frame
+
         end2 = time.time()
-        requests.post(mappingserver_addr, ujson.dumps({'id': message.id, 'user': message.user}))
+        #requests.post(mappingserver_addr, ujson.dumps({'id': message.id, 'user': message.user}))
         end3 = time.time()
 
         queue2.append(message)
@@ -164,10 +145,6 @@ def work1(condition1, condition2, SuperPointAndGlue, queue, queue2):
         condition2.notifyAll()
         condition2.release()
 
-        # thtemp =  threading.Thread(target=supergluematch2, args=(SuperPointAndGlue, id))
-        # thtemp.start()
-        #print("Message Processing = %s %d : %d : %f : " % (message.map, message.id, len(MapData[message.map].Frames), end3 - start))
-        #print("Message Processing = %d : %d : %f : %f : %f %f %f %d" % (id, len(FrameData), time1-start, time2-time1, end1 - time2, end2 - end1, end3-end2, len(queue)))
     print("End Message Processing Thread")
 
 #######################################################
@@ -612,99 +589,11 @@ if __name__ == "__main__":
         '--MAP', type=str,
         help='load map name')
 
-    # super glue and point
-    parser.add_argument(
-        '--input', type=str, default='0',
-        help='ID of a USB webcam, URL of an IP camera, '
-             'or path to an image directory or movie file')
-    parser.add_argument(
-        '--output_dir', type=str, default=None,
-        help='Directory where to write output frames (If None, no output)')
-
-    parser.add_argument(
-        '--image_glob', type=str, nargs='+', default=['*.png', '*.jpg', '*.jpeg'],
-        help='Glob if a directory of images is specified')
-    parser.add_argument(
-        '--skip', type=int, default=1,
-        help='Images to skip if input is a movie or directory')
-    parser.add_argument(
-        '--max_length', type=int, default=1000000,
-        help='Maximum length if input is a movie or directory')
-    parser.add_argument(
-        '--resize', type=int, nargs='+', default=[640, 480],
-        help='Resize the input image before running inference. If two numbers, '
-             'resize to the exact dimensions, if one number, resize the max '
-             'dimension, if -1, do not resize')
-
-    parser.add_argument(
-        '--superglue', choices={'indoor', 'outdoor'}, default='indoor',
-        help='SuperGlue weights')
-    parser.add_argument(
-        '--max_keypoints', type=int, default=500,
-        help='Maximum number of keypoints detected by Superpoint'
-             ' (\'-1\' keeps all keypoints)')
-    parser.add_argument(
-        '--keypoint_threshold', type=float, default=0.005,
-        help='SuperPoint keypoint detector confidence threshold')
-    parser.add_argument(
-        '--nms_radius', type=int, default=4,
-        help='SuperPoint Non Maximum Suppression (NMS) radius'
-             ' (Must be positive)')
-    parser.add_argument(
-        '--sinkhorn_iterations', type=int, default=20,
-        help='Number of Sinkhorn iterations performed by SuperGlue')
-    parser.add_argument(
-        '--match_threshold', type=float, default=0.2,
-        help='SuperGlue match threshold')
-
-    parser.add_argument(
-        '--show_keypoints', action='store_true',
-        help='Show the detected keypoints')
-    parser.add_argument(
-        '--no_display', action='store_true',
-        help='Do not display images to screen. Useful if running remotely')
-    parser.add_argument(
-        '--force_cpu', action='store_true',
-        help='Force pytorch to run in CPU mode.')
-
     opt = parser.parse_args()
     device0 = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
     # device3 = torch.device("cuda:3") if torch.cuda.is_available() else torch.device("cpu")
 
-    ###LOAD MIDAS
-    """
-    midas = torch.hub.load("intel-isl/MiDaS", "MiDaS")
-    midas.to(device0)
-    midas.eval()
-    midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
-    transform = midas_transforms.default_transform
-    """
-    ##LOAD SuperGlue & SuperPoint
-    if len(opt.resize) == 2 and opt.resize[1] == -1:
-        opt.resize = opt.resize[0:1]
-    if len(opt.resize) == 2:
-        print('Will resize to {}x{} (WxH)'.format(
-            opt.resize[0], opt.resize[1]))
-    elif len(opt.resize) == 1 and opt.resize[0] > 0:
-        print('Will resize max dimension to {}'.format(opt.resize[0]))
-    elif len(opt.resize) == 1:
-        print('Will not resize images')
-    else:
-        raise ValueError('Cannot specify more than two integers for --resize')
 
-    config = {
-        'superpoint': {
-            'nms_radius': opt.nms_radius,
-            'keypoint_threshold': opt.keypoint_threshold,
-            'max_keypoints': opt.max_keypoints
-        },
-        'superglue': {
-            'weights': opt.superglue,
-            'sinkhorn_iterations': opt.sinkhorn_iterations,
-            'match_threshold': opt.match_threshold,
-        }
-    }
-    matching = Matching(config).eval().to(device0)
     # matching=[]
     # for i in range(NUM_MAX_MATCH):
     #    matching.append(Matching(config).eval().to(device0))
@@ -742,7 +631,7 @@ if __name__ == "__main__":
     if opt.MAP:
         StartMap(opt.MAP)
 
-    th1 = threading.Thread(target=work1, args=(ConditionVariable, ConditionVariable2, matching, messages, messages2))
+    th1 = threading.Thread(target=work1, args=(ConditionVariable, ConditionVariable2, messages, messages2))
     th1.start()
     th2 = threading.Thread(target=work2, args=(ConditionVariable2, messages2))
     th2.start()
