8091fcca8cc7ac7d8e4bf14d808e34e98bc6cfc1
zkrkwlek_uvr <zkrkwlek@gmail.com>
2021-03-18 17:07:54 +0900

    #210318

--- Server.py
+++ Server.py
@@ -9,6 +9,9 @@ import cv2
 import argparse
 import torch
 
+from superglue.matching import Matching
+from superglue.utils import (frame2tensor, keyframe2tensor)
+
 #WSGI
 from gevent.pywsgi import WSGIServer
 
@@ -32,19 +35,22 @@ def work(cv,  mapqueue, framequeue, dataqueue, addr):
         cv.release()
         # 처리 시작
         start = time.time()
+
         img_array = np.frombuffer(data, dtype=np.uint8)
         img_cv = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        input_batch = transform(img_cv).to(device)
-        with torch.no_grad():
-            prediction = midas(input_batch)
-            prediction = torch.nn.functional.interpolate(
-                prediction.unsqueeze(1),
-                size=img_cv.shape[:2],
-                mode="bicubic",
-                align_corners=False,
-            ).squeeze().cpu().numpy()
+        img = cv2.cvtColor(img_cv, cv2.COLOR_RGB2GRAY)
+        frame_tensor = frame2tensor(img, device)
 
-        requests.post(addr + "?map=" + map + "&id="+id+"&key="+keyword, bytes(prediction))
+        time1 = time.time()
+        last_data = matching.superpoint({'image': frame_tensor})
+        time2 = time.time()
+        desc = last_data['descriptors'][0].cpu().detach().numpy()
+        kpts = last_data['keypoints'][0].cpu().detach().numpy()
+        # scores = last_data['scores'][0].cpu().detach().numpy()
+        end1 = time.time()
+
+        requests.post(addr + "?map=" + map + "&id="+id+"&key=bkpst", kpts.tobytes())
+        requests.post(addr + "?map=" + map + "&id=" + id + "&key=bdesc", desc.transpose().tobytes())
         end = time.time()
         print("Depth Processing = %s : %f : %d"%(id, end-start, len(dataqueue)))
 
@@ -82,15 +88,91 @@ if __name__ == "__main__":
         '--FACADE_SERVER_ADDR', type=str,
         help='facade server address')
 
+    # super glue and point
+    parser.add_argument(
+        '--input', type=str, default='0',
+        help='ID of a USB webcam, URL of an IP camera, '
+             'or path to an image directory or movie file')
+    parser.add_argument(
+        '--output_dir', type=str, default=None,
+        help='Directory where to write output frames (If None, no output)')
+
+    parser.add_argument(
+        '--image_glob', type=str, nargs='+', default=['*.png', '*.jpg', '*.jpeg'],
+        help='Glob if a directory of images is specified')
+    parser.add_argument(
+        '--skip', type=int, default=1,
+        help='Images to skip if input is a movie or directory')
+    parser.add_argument(
+        '--max_length', type=int, default=1000000,
+        help='Maximum length if input is a movie or directory')
+    parser.add_argument(
+        '--resize', type=int, nargs='+', default=[640, 480],
+        help='Resize the input image before running inference. If two numbers, '
+             'resize to the exact dimensions, if one number, resize the max '
+             'dimension, if -1, do not resize')
+
+    parser.add_argument(
+        '--superglue', choices={'indoor', 'outdoor'}, default='indoor',
+        help='SuperGlue weights')
+    parser.add_argument(
+        '--max_keypoints', type=int, default=500,
+        help='Maximum number of keypoints detected by Superpoint'
+             ' (\'-1\' keeps all keypoints)')
+    parser.add_argument(
+        '--keypoint_threshold', type=float, default=0.005,
+        help='SuperPoint keypoint detector confidence threshold')
+    parser.add_argument(
+        '--nms_radius', type=int, default=4,
+        help='SuperPoint Non Maximum Suppression (NMS) radius'
+             ' (Must be positive)')
+    parser.add_argument(
+        '--sinkhorn_iterations', type=int, default=20,
+        help='Number of Sinkhorn iterations performed by SuperGlue')
+    parser.add_argument(
+        '--match_threshold', type=float, default=0.2,
+        help='SuperGlue match threshold')
+
+    parser.add_argument(
+        '--show_keypoints', action='store_true',
+        help='Show the detected keypoints')
+    parser.add_argument(
+        '--no_display', action='store_true',
+        help='Do not display images to screen. Useful if running remotely')
+    parser.add_argument(
+        '--force_cpu', action='store_true',
+        help='Force pytorch to run in CPU mode.')
+
+
     opt = parser.parse_args()
     device = torch.device("cuda:"+opt.use_gpu) if torch.cuda.is_available() else torch.device("cpu")
 
-    ###LOAD MIDAS
-    midas = torch.hub.load("intel-isl/MiDaS", "MiDaS")
-    midas.to(device)
-    midas.eval()
-    midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
-    transform = midas_transforms.default_transform
+    ##LOAD SuperGlue & SuperPoint
+    if len(opt.resize) == 2 and opt.resize[1] == -1:
+        opt.resize = opt.resize[0:1]
+    if len(opt.resize) == 2:
+        print('Will resize to {}x{} (WxH)'.format(
+            opt.resize[0], opt.resize[1]))
+    elif len(opt.resize) == 1 and opt.resize[0] > 0:
+        print('Will resize max dimension to {}'.format(opt.resize[0]))
+    elif len(opt.resize) == 1:
+        print('Will not resize images')
+    else:
+        raise ValueError('Cannot specify more than two integers for --resize')
+
+    config = {
+        'superpoint': {
+            'nms_radius': opt.nms_radius,
+            'keypoint_threshold': opt.keypoint_threshold,
+            'max_keypoints': opt.max_keypoints
+        },
+        'superglue': {
+            'weights': opt.superglue,
+            'sinkhorn_iterations': opt.sinkhorn_iterations,
+            'match_threshold': opt.match_threshold,
+        }
+    }
+    matching = Matching(config).eval().to(device)
 
     maps = []
     datas = []
