d9087a6d8a8d12887adc1981f6c44853a5b96904
zkrkwlek_uvr <zkrkwlek@gmail.com>
2021-03-22 17:43:53 +0900

    #210322

--- Server.py
+++ Server.py
@@ -20,7 +20,29 @@ app = Flask(__name__)
 #cors = CORS(app)
 #CORS(app, resources={r'*': {'origins': ['143.248.96.81', 'http://localhost:35005']}})
 
-#work에서 호출하는 cv가 필요함.
+##model method
+from lib.utils.net_tools import load_ckpt
+from lib.utils.logging import setup_logging
+import torchvision.transforms as transforms
+from tools.parse_arg_test import TestOptions
+from lib.models.metric_depth_model import MetricDepthModel
+from lib.core.config import cfg, merge_cfg_from_file
+from lib.models.image_transfer import bins_to_depth
+
+def scale_torch(img, scale):
+    """
+    Scale the image and output it in torch.tensor.
+    :param img: input image. [C, H, W]
+    :param scale: the scale factor. float
+    :return: img. [C, H, W]
+    """
+    img = np.transpose(img, (2, 0, 1))
+    img = img[::-1, :, :]
+    img = img.astype(np.float32)
+    img /= scale
+    img = torch.from_numpy(img.copy())
+    img = transforms.Normalize(cfg.DATASET.RGB_PIXEL_MEANS, cfg.DATASET.RGB_PIXEL_VARS)(img)
+    return img
 
 import os
 def processingthread():
@@ -31,7 +53,25 @@ def processingthread():
         message = processqueue.pop()
         ConditionVariable2.release()
         # 처리 시작
+        with torch.no_grad():
+            img_array = np.frombuffer(message.data, dtype=np.uint8)
+            img_cv = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            img_resize = cv2.resize(img_cv, (int(img_cv.shape[1]), int(img_cv.shape[0])), interpolation=cv2.INTER_LINEAR)
+            img_torch = scale_torch(img_resize, 255)
+            img_torch = img_torch[None, :, :, :].cuda()
 
+            _, pred_depth_softmax = model.module.depth_model(img_torch)
+            pred_depth = bins_to_depth(pred_depth_softmax)
+            pred_depth = pred_depth.cpu().numpy().squeeze()
+            #print(pred_depth)
+            #pred_depth_scale = (pred_depth / pred_depth.max() * 60000).astype(
+            #    np.uint16)  # scale 60000 for visualization
+            #print(pred_depth.dtype)
+            #cv2.imwrite('./test.jpg', pred_depth)
+        requests.post(FACADE_SERVER_ADDR + "/ReceiveData?map=" + message.map + "&id=" + message.id + "&key=bdepth",
+                      bytes(pred_depth))
+        requests.post(PROCESS_SERVER_ADDR + "/notify", ujson.dumps(
+                {'user': message.user, 'map': message.map, 'id': int(message.id), 'key': 'bdepth'}))
         # processing end
 
 def datathread():
@@ -73,6 +113,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='WISE UI Web Server',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser =  TestOptions().initialize(parser)
+    #parser = TestOptions().parse()
+
+    parser.thread = 1
+    parser.batchsize = 1
     parser.add_argument(
         '--ip', type=str,default='0.0.0.0',
         help='ip address')
@@ -95,8 +140,21 @@ if __name__ == "__main__":
         '--PROCESS_SERVER_ADDR', type=str,
         help='process server address')
 
+    ##module args
     opt = parser.parse_args()
+    merge_cfg_from_file(opt)
     device = torch.device("cuda:"+opt.use_gpu) if torch.cuda.is_available() else torch.device("cpu")
+    #load model
+
+
+    # load model
+    model = MetricDepthModel()
+    # load checkpoint
+    if opt.load_ckpt:
+        load_ckpt(opt, model)
+    #model.cuda()
+    model.eval().to(device)
+    model = torch.nn.DataParallel(model)
 
     dataqueue = []
     processqueue = []
@@ -114,7 +172,7 @@ if __name__ == "__main__":
     #app.run(host=opt.ip, port=opt.port)
     #app.run(host=opt.ip, port = opt.port, threaded = True)
 
-    keyword = 'bdepth'
+    keyword = 'depth'
     requests.post(FACADE_SERVER_ADDR + "/ConnectServer", ujson.dumps({
         'port':opt.port,'key': keyword, 'prior':opt.prior, 'ratio':opt.ratio
     }))
