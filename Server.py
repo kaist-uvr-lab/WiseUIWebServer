44b2aa0221ad279add12257a0e2a28567ba9310a
zkrkwlek_uvr <zkrkwlek@gmail.com>
2021-03-27 17:10:39 +0900

    #remove echo server code in facade branch

--- Server.py
+++ Server.py
@@ -27,38 +27,6 @@ from module.Message import Message
 from gevent.pywsgi import WSGIServer
 from gevent import monkey
 
-###############################
-#Async Echo Server
-###############################
-import asyncio
-
-async def handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-    while True:
-        # 클라이언트가 보낸 내용을 받기
-        data: bytes = await reader.read(1024)
-        """
-        # 받은 내용을 출력하고,
-        # 가공한 내용을 다시 내보내기
-        peername = writer.get_extra_info('peername')
-        print(f"[S] received: {len(data)} bytes from {peername}")
-        mes = data.decode()
-        print(f"[S] message: {mes}")
-        res = mes.upper()[::–1]
-        await asyncio.sleep(random() * 2)
-        writer.write(res.encode())
-        await writer.drain()
-        """
-
-async def main():
-    await asyncio.wait([run_server()])
-
-async def run_server():
-    # 서버를 생성하고 실행
-    server = await asyncio.start_server(handler, host="0.0.0.0", port=35001)
-    async with server:
-        # serve_forever()를 호출해야 클라이언트와 연결을 수락합니다.
-        await server.serve_forever()
-
 ##################################################
 # API part
 ##################################################
