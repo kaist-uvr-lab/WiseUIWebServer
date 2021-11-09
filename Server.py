8283487a29cc3b40cd9177027055bb91766d3ed7
zkrkwlek <zkrkwlek@gmail.com>
2021-11-09 17:09:24 +0900

    #211109

--- Server.py
+++ Server.py
@@ -10,6 +10,7 @@ from flask_cors import CORS
 import base64
 import cv2
 
+import datetime
 import argparse
 import torch
 import os
@@ -29,6 +30,13 @@ from socket import *
 ####WSGI
 from gevent.pywsgi import WSGIServer
 from gevent import monkey
+
+####UDP for notification
+def udpthread():
+    while True:
+        print("asdf")
+
+####UDP for notification
 
 ##################################################
 # API part
@@ -51,6 +59,7 @@ def Connect():
         if keyword not in Keywords:
             Keywords.add(keyword)
             Data[keyword] = {}
+            Data["TS"][keyword]={}
             """
             if type2 == "raw":
                 Data[keyword]['id'] = int(0)
@@ -59,6 +68,7 @@ def Connect():
             """
     return 'a'
 
+epoch=datetime.datetime(1970,1,1,0,0,0,0)
 @app.route("/Store", methods=['POST'])
 def Store():
 
@@ -67,37 +77,32 @@ def Store():
     id2 = int(request.args.get('id2',-1))
     src = request.args.get('src')
     type2 = request.args.get('type2','None')
-    """
-    if keyword == 'Matching':
-        print(type2)
-    if keyword == "Matches":
-        print("1 Matches id %d %d"%(id1, id2))
-    """
+
+    #ts
+    current_datetime = datetime.datetime.utcnow()-epoch
+    ts = current_datetime.total_seconds()
+    #current_timetuple = current_datetime.utctimetuple()
+    #current_timestamp = calendar.timegm(current_timetuple)
+
     if keyword in Keywords:
         if Data[keyword].get(src) is None:
             Data[keyword][src] = {}
-        """
-        tid = int(Data[keyword]['id']+1)
-        if tid == 0:
-            tid = id1
-        else:
-            Data[keyword]['id'] = tid
-        id1 = tid
-        """
-        """
-        Data[keyword]['src'] = src  # data['src']
-        if id2 >= 0:
-            id = str(tid)+","+str(id2)
-            Data[keyword][id] = request.data
-            json_str = {'keyword': keyword, 'type1': 'notification', 'id': tid, 'id2':id2, 'type2':type2, 'src': src}
-        else:
-       """
+            Data["TS"][keyword][src] = {}
+
         Data[keyword][src][id1] = request.data
+        Data["TS"][keyword][src][id1] =ts
+
+        if keyword == "MappingResult" or keyword == "ObjectDetection" or keyword == "ReferenceFrame":
+            span = Data["TS"][keyword][src][id1]-Data["TS"]["Image"][src][id1]
+            print("image span = %s = %f = %f %f"%(keyword, span, Data["TS"][keyword][src][id1],Data["TS"]["Image"][src][id1]))
+
         if id2 == -1:
-            json_str = {'keyword': keyword, 'type1': 'notification', 'type2':type2, 'id': id1, 'src': src}
+            json_str = {'keyword': keyword, 'type1': 'notification', 'type2':type2, 'id': id1, 'src': src, 'ts':ts}
         else:
+            print("????????????????")
             json_str = {'keyword': keyword, 'type1': 'notification', 'type2': type2, 'id': id1, 'id2':id2, 'src': src}
         json_data = ujson.dumps(json_str)
+
         udp_manage_soc.sendto(json_data.encode(), CONTENT_ECHO_SERVER_ADDR)
         #print(data['data'])
     return 'a'#str(id1).encode()
@@ -109,12 +114,13 @@ def Load():
     id2 = int(request.args.get('id2', -1))
     src = request.args.get('src')
     if keyword in Keywords:
-        """
-        if id2 >= 0:
-            id = str(id1) + "," + str(id2)
-            return bytes(Data[keyword][id])
-        else:
-        """
+
+        if keyword == "MappingResult":
+            current_datetime = datetime.datetime.utcnow() - epoch
+            ts = current_datetime.total_seconds()
+            span = ts-Data["TS"]["Image"][src][id1]
+            print("Load -store time span = %s = %f = %f %f"%(keyword, span, Data["TS"][keyword][src][id1],Data["TS"]["Image"][src][id1]))
+
         return bytes(Data[keyword][src][id1])
     return ''
 ###########################################################################################################################
@@ -184,6 +190,7 @@ if __name__ == "__main__":
     #UserData = {}
     #MapData = {}
     Data={}
+    Data["TS"] = {}
     Keywords = set()
     nKeywordID = 0
 
@@ -197,8 +204,12 @@ if __name__ == "__main__":
     CONTENT_ECHO_SERVER_ADDR = (opt.CONTENT_IP, opt.CONTENT_PORT)
     udp_manage_soc = socket(AF_INET, SOCK_DGRAM)
 
+    th1 = threading.Thread(target=udpthread)
+    th1.start()
+
     print('Starting the API')
     # app.run(host=opt.ip, port=opt.port)
     # app.run(host=opt.ip, port = opt.port, threaded = True)
     http = WSGIServer((opt.ip, opt.port), app.wsgi_app)
     http.serve_forever()
+
